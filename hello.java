6883aa03b006d0fa24cca7ade09cd46c8216d970
Sarthak Sonar <sarthak.sonar_24uds@sanjivani.edu.in>
2024-12-27 22:52:52 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public static void main (String [] args )
   {
     String ABC = "Hello World! ";
     System.out.println(ABC);
-    for (int i=0; i<=10; i++)
+    for (int i=2; i<=20; i+=2)
       {
         System.out.println(i);
       }
