35a431843832fe4c95f6de091c8559aead8fd221
Sarthak Sonar <sarthak.sonar_24uds@sanjivani.edu.in>
2024-12-29 23:53:55 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,8 @@ public static void main (String [] args )
   {
     String ABC = "Sarthak sonar ";
     System.out.println(ABC);
+    String AB = "Hello world ";
+    System.out.println(AB);
     for (int i=2; i<=20; i+=2)
       {
         System.out.println(i);
